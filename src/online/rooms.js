404f19323d989c987e98d8d357c12c8c70fdfdc2
jaworskihubert1-spec <jaworski.hubert1@gmail.com>
2026-05-15 00:45:44 +0200

    Dalsze prace

---
+++ src/online/rooms.js
@@ -0,0 +1,40 @@
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDoc,
+  doc,
+} from "firebase/firestore";
+
+import app from "../firebase";
+
+const db = getFirestore(app);
+
+export async function createRoom(hostName) {
+  const roomRef = await addDoc(collection(db, "rooms"), {
+    host: hostName,
+    createdAt: Date.now(),
+    status: "waiting",
+    players: [
+      {
+        name: hostName,
+        ready: false,
+      },
+    ],
+  });
+
+  return roomRef.id;
+}
+
+export async function getRoom(roomId) {
+  const roomDoc = await getDoc(doc(db, "rooms", roomId));
+
+  if (!roomDoc.exists()) {
+    return null;
+  }
+
+  return {
+    id: roomDoc.id,
+    ...roomDoc.data(),
+  };
+}
